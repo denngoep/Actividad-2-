2c0c69f4b1df1e1f27995c3f9b01a4b8f511b587
Dennis Gonzalez <degonzalez376@gmail.com>
2026-02-18 19:38:30 -0500

    Primer commit

---
+++ Proyecto 2/Program.cs
@@ -0,0 +1,18 @@
+﻿static string CheckNumber(int numero)
+{
+    if (numero > 0)
+    {
+        return "El número es positivo";
+    }
+    else if (numero < 0)
+    {
+        return "El número es negativo";
+    }
+    else
+    {
+        return "El número es cero";
+    }
+}
+Console.WriteLine("Ingrese un número:");
+int numeroIngresado = int.Parse(Console.ReadLine());
+Console.WriteLine(CheckNumber(numeroIngresado));
